e5b6641f79f3ebc7876b053276db5cc4cea72927
MOHAMAD ALI ALCHIHABI <mohamadali.business@gmail.com>
2026-05-07 23:53:34 +0300

    chore: update GitHub Actions workflow and Svelte config for npm usage

--- svelte.config.js
+++ svelte.config.js
@@ -7,10 +7,7 @@ const config = {
 			fallback: '404.html'
 		}),
 		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
-		},
-		prerender: {
-			handleMissingId: 'warn' // Suppress missing ID errors
+			base: process.argv.includes('dev') ? '' : '/lutof-website'
 		}
 	}
 };
