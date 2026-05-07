4d12903166dc48f9c194c82f54edfcdaa7c58bfd
MOHAMAD ALI ALCHIHABI <mohamadali.business@gmail.com>
2026-05-07 23:57:09 +0300

    chore: update BASE_PATH and GitHub Pages URL for deployment configuration

--- svelte.config.js
+++ svelte.config.js
@@ -7,7 +7,7 @@ const config = {
 			fallback: '404.html'
 		}),
 		paths: {
-			base: process.argv.includes('dev') ? '' : '/lutof-website'
+			base: process.argv.includes('dev') ? '' : ''
 		}
 	}
 };
